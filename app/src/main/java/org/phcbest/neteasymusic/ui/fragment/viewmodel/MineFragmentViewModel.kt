0c9a57b433bbb6bfe3334795d02d38179b8d71ee
PengHaiChen <phcbest2017@outlook.com>
2022-04-17 12:37:41 +0800

    将viewbinding切换成databinding

---
+++ app/src/main/java/org/phcbest/neteasymusic/ui/fragment/viewmodel/MineFragmentViewModel.kt
@@ -0,0 +1,11 @@
+package org.phcbest.neteasymusic.ui.fragment.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MineFragmentViewModel : ViewModel {
+
+    constructor() {
+        //联网访问用户信息
+
+    }
+}
