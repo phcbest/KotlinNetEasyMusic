d16d7897cdf023a6d5e78ad2299ebc8918f8adbd
PengHaiChen <phcbest2017@outlook.com>
2022-05-25 13:12:12 +0800

    更新为高级idea版本

--- app/src/main/java/org/phcbest/neteasymusic/ui/fragment/CloudVillageFragment.kt
+++ app/src/main/java/org/phcbest/neteasymusic/ui/fragment/CloudVillageFragment.kt
@@ -1,28 +1,38 @@
 package org.phcbest.neteasymusic.ui.fragment
 
+import android.database.DatabaseUtils
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.viewbinding.ViewBinding
 import org.phcbest.neteasymusic.R
+import org.phcbest.neteasymusic.base.BaseFragment
+import org.phcbest.neteasymusic.databinding.FragmentCloudVillageBinding
 
 
-class CloudVillageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class CloudVillageFragment : BaseFragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_cloud_village, container, false)
+
+    override fun initPresenter() {
+    }
+
+    override fun initView() {
+    }
+
+    override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): ViewBinding {
+        val view = DataBindingUtil.inflate<FragmentCloudVillageBinding>(inflater,
+            R.layout.fragment_cloud_village,
+            container,
+            false)
         return view
     }
 
--- app/src/main/java/org/phcbest/neteasymusic/ui/fragment/FollowFragment.kt
+++ app/src/main/java/org/phcbest/neteasymusic/ui/fragment/FollowFragment.kt
@@ -1,35 +1,18 @@
 package org.phcbest.neteasymusic.ui.fragment
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.viewbinding.ViewBinding
 import org.phcbest.neteasymusic.R
+import org.phcbest.neteasymusic.base.BaseFragment
+import org.phcbest.neteasymusic.databinding.FragmentFollowBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class FollowFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class FollowFragment : BaseFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_follow, container, false)
-    }
 
     companion object {
         /**
@@ -37,5 +20,19 @@ class FollowFragment : Fragment() {
          */
         @JvmStatic
         fun newInstance() = FollowFragment()
+    }
+
+    override fun initPresenter() {
+    }
+
+    override fun initView() {
+    }
+
+    override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): ViewBinding {
+        val binding = DataBindingUtil.inflate<FragmentFollowBinding>(inflater,
+            R.layout.fragment_follow,
+            container,
+            false)
+        return binding
     }
 }
