0800ecdf2486653450aaa7b0f96bb65b61508c97
PengHaiChen <phcbest2017@outlook.com>
2022-06-22 22:38:58 +0800

    编写使用MediaSession的播放服务

--- app/src/main/java/org/phcbest/neteasymusic/service/SimulateClientActivity.kt
+++ app/src/main/java/org/phcbest/neteasymusic/service/SimulateClientActivity.kt
@@ -1,4 +1,33 @@
 package org.phcbest.neteasymusic.service
 
-class SimulateClientActivity {
+import android.content.ComponentName
+import android.media.browse.MediaBrowser
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SimulateClientActivity : AppCompatActivity() {
+
+    private var mBrowser: MediaBrowser? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBrowser = MediaBrowser(this, ComponentName(this, MusicPlayerMediaSession::class.java),
+            browserConnectionCallback, null)
+    }
+
+    private var browserConnectionCallback = object : MediaBrowser.ConnectionCallback() {
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //发送连接请求
+        mBrowser?.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //断开连接
+        mBrowser?.disconnect()
+    }
 }
