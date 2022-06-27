82644a573594635274ea527bf8753a744a060001
PengHaiChen <phcbest2017@outlook.com>
2022-06-27 21:54:32 +0800

    编写类retrofit框架

--- app/mini_retrofit/src/main/java/org/phcbest/mini_retrofit/MiniRetrofit.kt
+++ app/mini_retrofit/src/main/java/org/phcbest/mini_retrofit/MiniRetrofit.kt
@@ -1,7 +1,11 @@
 package org.phcbest.mini_retrofit
 
+import java.lang.IllegalArgumentException
+import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
+import java.lang.reflect.Proxy
 import java.util.concurrent.ConcurrentHashMap
+
 
 /**
  * 这个类是仿照retrofit的实现方法来进行开发的一个网络框架
@@ -26,7 +30,40 @@ class MiniRetrofit {
      */
     fun <T> create(sercice: Class<T>): T {
         //动态代理
-        TODO()
+        return Proxy.newProxyInstance(sercice.classLoader, arrayOf<Class<*>>(sercice),
+            object : InvocationHandler {
+                override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any {
+                    //1 验证是否接口
+                    require(sercice.isInterface) {
+                        throw IllegalArgumentException("API declarations must be interfaces.")
+                    }
+                    //2 进行构建接口实例
+
+                    TODO()
+                    //3 调用http框架进行处理
+                }
+            }) as T
+
+//        return Proxy.newProxyInstance(service.getClassLoader(), arrayOf<Class<*>>(service),
+//            object : InvocationHandler() {
+//                @Throws(Throwable::class)
+//                operator fun invoke(
+//                    proxy: Any?,
+//                    method: Method?,
+//                    args: Array<Any?>?,
+//                ): Any? {
+//
+//                    //1 验证是否是接口
+//                    require(service.isInterface()) { "API declarations must be interfaces." }
+//
+//                    //2 进行构建接口实例，同时进行方法注解、方法参数注解解析，这些参数解析完成以后，我们就可以把注解解析后的url进行拼接
+//                    val serviceMethod: ServiceMethod = loadServiceMethod(method, args)
+//
+//                    //3 调用 http 框架执行网络请求
+//                    return OkHttpCall(serviceMethod)
+//                }
+//            }
+//        ) as T
     }
 
 }
