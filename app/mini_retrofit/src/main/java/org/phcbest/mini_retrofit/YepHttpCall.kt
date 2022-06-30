21ce80d793dc417ba8989dfd3e7ea53b2465e8a0
PengHaiChen <phcbest2017@outlook.com>
2022-06-30 23:05:22 +0800

    编写类mini_retrofit框架,给model添加okhttp depend

--- app/mini_retrofit/src/main/java/org/phcbest/mini_retrofit/YepHttpCall.kt
+++ app/mini_retrofit/src/main/java/org/phcbest/mini_retrofit/YepHttpCall.kt
@@ -1,11 +1,13 @@
 package org.phcbest.mini_retrofit
 
+import okhttp3.OkHttpClient
+import sun.net.www.http.HttpClient
 import javax.security.auth.callback.Callback
 
 class YepHttpCall(private var serviceMethod: ServiceMethod) : Call {
 
     companion object {
-
+        var client: OkHttpClient = OkHttpClient()
     }
 
     override fun execute() {
